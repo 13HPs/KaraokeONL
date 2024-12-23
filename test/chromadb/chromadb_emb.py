4e424e9db4f95453ef24a053b78b897741b07d33
13HPs <huuhiep3723.hasasin@gmail.com>
2024-12-23 13:41:08 +0700

    up code

--- test/chromadb/chromadb_emb.py
+++
@@ -1,9 +0,0 @@
-from chromadb.utils import embedding_functions
-
-default_emb = embedding_functions.DefaultEmbeddingFunction()
-
-name = "Custom Embedding Function"
-
-emb = default_emb(name)
-
-print(emb)
